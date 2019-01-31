1362bc18a618d13b68a1e042c64ca6334c85306f
Tali Rotteveel <TRotteveel@rightpoint.com>
2019-01-30 18:31:15 -0600

    feb2019-2

--- resume.js
+++ resume.js
@@ -29,8 +29,22 @@ class Education{
         this.major = major;
     }
 }
-
-var relevantJobs = {
+var rightpointExperience = {
+    sectionOne: "Built out reporting solutions leveraging PowerBI to provide real time views of our financial and staffing data. ",
+    descriptionOne: {
+        bulletOne: "Built out complex tables, procedures, functions, and views in our databases.",
+        bulletTwo: "Created a projection of our revenue for the company based on a combination of our monthly forecast, historical actuals, and in-flight burn rate.",
+        bulletThree: "Create fast and flexible reports that are easily able to filter and sort so leaders can track actuals to goals.",
+        bulletFour: "Worked on refactoring many of our existing stored procedures to enhance performance and improve readability."
+    },
+    sectionTwo: "Worked on web applications utilizing C#, SQL, KnockoutJS, ASP.NET, Azure",
+    descriptionTwo: {
+        bulletOne: "Refactored existing code for performance enhancement and accuracy",
+        bulletTwo: "Built out new pages and features within an existing application.",
+        bulletThree: "Worked on an application to sync down large amounts of data from our Sales and Project Managment softwares to our databases."
+    }
+}
+var internExperience = {
     sectionOne: "Used ALM process development/documentation tools to plan and maintain project", 
     descriptionOne: {
         bulletOne: "Tackled website programming tasks (such as fixing bugs and adding features) using JavaScript, HTML, KnockOut, and C# using the .NET MVC framework.",
@@ -103,38 +117,25 @@ $.fn.SkillMeter = function(options) {
 
 
 
-const myResume = new Biography("Tali Rotteveel", "224-655-8058", "talirotteveel@hotmail.com", "Chicago", "IL");
-const rightpoint = new Job("Developer Intern", "Rightpoint Consulting", "10/2017 - present");
-const northside = new Job("Server", "Northside Bar and Grill", "04/2016 - 02/2017");
-const decero = new Job("Server", "De Cero", "06/2015 - 10/2016");
-const bluemartini = new Job("Hostess", "Blue Martini", "06/2014 - 07/2015");
-const beanz = new Job("Barista", "Beanz Coffee Cafe", "06/2013 - 07/2014");
-const school = new Education("Northeastern Illinois University", "2013-2017", "Computer Science");
+const myResume = new Biography("Tali Rotteveel", "224-655-8058", "talirotteveel4@gmail.com", "Chicago", "IL");
+const rightpointintern = new Job("Developer Intern", "Rightpoint Consulting", "10/2016 - 06/2017");
+const rightpoint = new Job("Software Developer", "Rightpoint Consulting", "06/2017 - present");
+const school = new Education("Northeastern Illinois University", "2013-2017", "Bachelor in Computer Science");
 
 document.getElementById("name").innerHTML = myResume.name;
 document.getElementById("phone-number").innerHTML = myResume.phoneNumber;
 document.getElementById("email-address").innerHTML = myResume.emailAddress;
 document.getElementById("location").innerHTML = myResume.location;
-document.getElementById("current-company").innerHTML = rightpoint.company;
-document.getElementById("current-dates").innerHTML = rightpoint.date;
-document.getElementById("current-position").innerHTML = rightpoint.position;
-document.getElementById("employer").innerHTML = print(relevantJobs);
+document.getElementById("intern-company").innerHTML = rightpointintern.company;
+document.getElementById("intern-dates").innerHTML = rightpointintern.date;
+document.getElementById("intern-position").innerHTML = rightpointintern.position;
+document.getElementById("intern-employer").innerHTML = print(internExperience);
+document.getElementById("developer-employer").innerHTML = print(rightpointExperience);
+document.getElementById("developer-position").innerHTML = rightpoint.position;
+document.getElementById("developer-company").innerHTML = rightpoint.company;
+document.getElementById("developer-dates").innerHTML = rightpoint.date;
 document.getElementById("school").innerHTML = school.institution;
 document.getElementById("dates").innerHTML = school.dates;
 document.getElementById("major").innerHTML = school.major;
 document.getElementById("courses").innerHTML = "Relevant courses: " + courses; 
 document.getElementById("testimonials").innerHTML = print(testimonials);
-
-
-document.getElementById("northside-company").innerHTML = northside.company;
-document.getElementById("northside-dates").innerHTML = northside.date;
-document.getElementById("northside-position").innerHTML = northside.position;
-document.getElementById("decero-company").innerHTML = decero.company;
-document.getElementById("decero-dates").innerHTML = decero.date;
-document.getElementById("decero-position").innerHTML = decero.position;
-document.getElementById("bluemartini-company").innerHTML = bluemartini.company;
-document.getElementById("bluemartini-dates").innerHTML = bluemartini.date;
-document.getElementById("bluemartini-position").innerHTML = bluemartini.position;
-document.getElementById("beanz-company").innerHTML = beanz.company;
-document.getElementById("beanz-dates").innerHTML = beanz.date;
-document.getElementById("beanz-position").innerHTML = beanz.position;
